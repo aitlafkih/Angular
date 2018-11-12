6e7391949142ca202d1d08ca8b1ab8e9bcd47dfc
amine <aitlafkih.amine@gmail.com>
2018-11-12 20:35:53 +0100

    add paramap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentsListComponent } from './departments-list/departments-list.component';
+import { EmploymentsListComponent } from './employments-list/employments-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'', redirectTo: '/departments', pathMatch: 'full'},
+  {path :'departments', component: DepartmentsListComponent},
+  {path :'departments/:id', component : DepartmentDetailComponent},
+  {path :'employees', component: EmploymentsListComponent },
+  {path :'**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DepartmentsListComponent,EmploymentsListComponent,PageNotFoundComponent, DepartmentDetailComponent];
---
+++ src/app/departments-list/departments-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,ParamMap} from '@angular/router';
+
+@Component({
+  selector: 'app-departments-list',
+  templateUrl: './departments-list.component.html',
+  styleUrls: ['./departments-list.component.css']
+})
+export class DepartmentsListComponent implements OnInit {
+
+  public selectedId;
+  departments = [
+    {"id": 1, "name": "angular"},
+    {"id": 2, "name": "devops"},
+    {"id": 3, "name": "admin"},
+    {"id": 4, "name": "l7alwa"}
+  ]
+
+  constructor(private router : Router, private route : ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.selectedId = id; 
+    })
+  }
+
+  onSelectDetails(department){
+    this.router.navigate(['/departments', department.id]);
+
+  }
+
+  isSelectedId(department){
+    return department.id === this.selectedId;
+
+  }
+
+
+}
